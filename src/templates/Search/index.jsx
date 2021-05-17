a2ab1d79a1371704be429611a4deac7c72b48ff9
Giosepe Luiz <giosepe_luiz3@live.com>
2021-05-17 16:20:02 -0300

    Search.jsx

--- src/templates/Search/index.jsx
+++ src/templates/Search/index.jsx
@@ -2,16 +2,50 @@ import { Component } from 'react';
 import './style.scss';
 
 class Search extends Component {
-  constructor(){
+  constructor() {
     super();
-    console.log('Constructor...')
+    console.log("Constructor...");
   }
-  
-  render(){
-    return(
+
+  state = {
+    names: ['Carregando nomes...'],
+    searchValue: ''
+  };
+
+  componentDidMount() {
+    this.handleListNames();
+  }
+
+  handleListNames = async () => {
+    const response = fetch("https://raw.githubusercontent.com/dominictarr/random-name/master/names.json");
+    const [names] = await Promise.all([response]);
+    const responseJson = await names.json();
+    this.setState({ names: responseJson });
+  };
+
+  handleSearch = (e) => {
+    const { value } = e.target;
+    this.setState({ searchValue: value });
+  }
+
+  render() {
+    const { names, searchValue } = this.state;
+    
+    const filteredNames = !!searchValue ? names.filter(name => (
+      name.toLowerCase().includes(
+        searchValue.toLowerCase()
+      )
+    )) : names;
+
+    return  (
+      
       <div className="container">
-        Olá Mundo!!!
+        <input className="inputtext" value={ searchValue } onChange={this.handleSearch} type="text" placeholder="Digite um nome para busca" /><br /><br />
+        { filteredNames.map(name => (
+          <p>{name}</p>
+        )) }
       </div>
+
     )
   }
 }

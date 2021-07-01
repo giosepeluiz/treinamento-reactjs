7f29eb21cbb2c598c12a63e270ea4a14fa452cbf
Giosepe Luiz <giosepe_luiz3@live.com>
2021-07-01 16:10:10 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ import './styles/global-styles.scss';
 //import SearchHook from './templates/SearchHook';
 //import ImcCalc from './templates/ImcCalc';
 //import Countries from './templates/Countries';
-import GameList from './templates/GameList';
+//import GameList from './templates/GameList';
+import Fruit from './templates/Fruit';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GameList />
+    <Fruit />
   </React.StrictMode>,
   document.getElementById('root'),
 );
